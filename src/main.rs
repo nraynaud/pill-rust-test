dd87c0756314bd098da3f91d51292165257549ab
Nicolas Raynaud <github@nraynaud.com>
2019-01-13 21:59:32 -0700

    test pwm

--- src/main.rs
+++ src/main.rs
@@ -6,36 +6,30 @@
 
 extern crate panic_halt;
 
+use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::hprintln;
-use nb::block;
 use stm32f103xx_hal::{
     device,
     prelude::*,
-    timer::Timer,
 };
 
 #[entry]
 fn main() -> ! {
-    hprintln!("hello").unwrap();
     let _cp = cortex_m::Peripherals::take().unwrap();
     let dp = device::Peripherals::take().unwrap();
-
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-
-    // Try a different clock configuration
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
-
-    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
-    // Try a different timer (even SYST)
-    let mut timer = Timer::tim1(dp.TIM1, 1.hz(), clocks, &mut rcc.apb2);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    gpioa.pa8.into_alternate_open_drain(&mut gpioa.crh);
+    // prevent use
+    let _led = gpioc.pc13.into_floating_input(&mut gpioc.crh);
+    let mut pwm = dp.TIM2.pwm(gpioa.pa0.into_alternate_push_pull(&mut gpioa.crl), &mut afio.mapr, 2.hz(), clocks, &mut rcc.apb1);
+    pwm.enable();
+    pwm.set_duty(pwm.get_max_duty() / 2);
     loop {
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
-        led.set_low();
+        asm::wfi();
     }
 }
